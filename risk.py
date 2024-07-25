a9494f2ad03bfe0e44b888e4cfc7c3ec13afdce1
Niyyet <110846058+rza4ev@users.noreply.github.com>
2024-07-25 18:30:34 +0400

    Update risk.py

--- risk.py
+++ risk.py
@@ -38,7 +38,7 @@ process_frequency_map = {
     'Daily': 0, 'Monthly': 1, 'Quarterly': 2, 'Weekly': 3, 'Yearly': 4
 }
 
-model_file = 'C:/Users/MSI GF75/Desktop/Niyyat/IrshadEelec/linear.joblib'
+model_file = 'linear.joblib'
 
 # Check if the model file exists
 if os.path.isfile(model_file):
